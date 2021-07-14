47649c81e23539eaa52321ef8d9c24f280f0cfc3
binomial0418 <39080023+binomial0418@users.noreply.github.com>
2021-07-14 14:09:25 +0800

    新增寫入thingspeak

--- tgs_sensor_read_analog.ino
+++ tgs_sensor_read_analog.ino
@@ -6,10 +6,14 @@
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 
 // wifi
-const char* ssid     = "XXXX";
-const char* password = "xxxx";
+const char* ssid     = "ap";
+const char* password = "password";
 ESP8266WiFiMulti WiFiMulti;
 WiFiClient client;
+//For ThingSpeak
+const char* apiKey = "token";
+const char* resource = "/update?api_key=";
+const char* server = "api.thingspeak.com";
 
 //一些全域變數
 int iLinkNetWork=0;
@@ -41,10 +45,13 @@ void loop()
 { 
   int data = tgs_analog_read();
   Serial.println("data :" + String(data));
-  //寫入DB using ws
   if (data > 50) {
+    //寫入Thingspeak
+    wrt_to_thingspeak(data);
+    //寫入DB using ws
     send_data_by_php("TGS-1",String(data));
   }
+  
   delay(60000);
 }
 
@@ -56,18 +63,48 @@ long tgs_analog_read() {
   return sensorValue;
 }
 
+//寫資料到Thingspeak
+void wrt_to_thingspeak(int data){
+  int iLnk = 0;
+  String sData;
+  // 使用 80 Port 連線
+  if (client.connect(server, 80)) {
+  //  iLnk = 1;
+  } 
+  sData   = String(data);
+  client.print(String("GET ") + resource + apiKey + "&field1=" + sData +
+               " HTTP/1.1\r\n" +
+               "Host: " + server + "\r\n" + 
+               "Connection: close\r\n\r\n");
+                  
+  int timeout = 5 * 10; // 5 seconds             
+  while(!client.available() && (timeout-- > 0)){
+    delay(1000);
+  }
+  
+  if(!client.available()) {
+     Serial.println(F("No response, going back to sleep"));
+  }
+  while(client.available()){
+    Serial.write(client.read());
+  }
+  
+  Serial.println(F("\nclosing connection"));
+  client.stop();
+}
+
 //用WS寫入MySql
 void send_data_by_php(String dev_typ,String val){
   int iLnk = 0;
   String s;
  
-  if (client.connect("dns", 80)) {
+  if (client.connect("address", 8088)) {
     iLnk = 1;
   }
   while(iLnk ==0){
     delay(500);
     Serial.print(F("\nreconnect php srv"));
-    if (client.connect("dns", 80)) {
+    if (client.connect("address", 8088)) {
       iLnk = 1;
     }   
   }
